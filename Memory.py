9f406f9da6d136ba4b8e7c2645febc80822eb766
NomNom12328 <62714997+NomNom12328@users.noreply.github.com>
2020-04-27 16:26:52 +0200

    Add files via upload

--- Memory.py
+++ Memory.py
@@ -1,11 +1,11 @@
-
+import engine #umetanje engine, napravio ga je Leonardo
 import pygame #umetanje pygamea
 pygame.init() 
 white=(255, 255, 255) #odredivanje boje za pozadinu
 #odredivanje velicine ekrana
 X = 1200 
 Y = 600
-display_surface = pygame.display.set_mode((X, Y ))
+dis = pygame.display.set_mode((X, Y ))
 #umetanje slika
 pygame.display.set_caption('Image')
 red=pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\red.jpg')
@@ -43,15 +43,15 @@ display_surface.blit(e, (600, 300))
 display_surface.blit(f, (900, 10))
 display_surface.blit(g, (900, 300))
 #postavljanje drge strane 'kartice' preko slika tako da ih igrac ne vidi
-display_surface.blit(red, (10, 10))
-display_surface.blit(red, (10, 300))
-display_surface.blit(red, (300, 10))
-display_surface.blit(red, (600, 10))
-display_surface.blit(red, (300, 300))
-display_surface.blit(red, (600, 300))
-display_surface.blit(red, (900, 10))
-display_surface.blit(red, (900, 300))
 
+engine.buttonDrawImage(dis, 10, 10, red)
+engine.buttonDrawImage(dis, 10, 300, red)
+engine.buttonDrawImage(dis, 300, 10, red)
+engine.buttonDrawImage(dis, 600, 10, red)
+engine.buttonDrawImage(dis, 300, 300, red)
+engine.buttonDrawImage(dis, 600, 300, red)
+engine.buttonDrawImage(dis, 900, 10, red)
+engine.buttonDrawImage(dis, 900, 300, red)
 
 #izlazak iz programa
 for event in pygame.event.get() : 
@@ -60,10 +60,6 @@ for event in pygame.event.get() :
         quit()
     pygame.display.update()
 
-if event.type == pygame.MOUSEBUTTONDOWN:
-            # Set the x, y postions of the mouse click
-            x, y = event.pos
-            if red.get_rect().collidepoint(x, y):
-                del red
+
 
  
