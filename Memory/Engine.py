2f1aa0a4cefead0c566da936dfbc517f26e5b5d6
NomNom12328 <62714997+NomNom12328@users.noreply.github.com>
2020-04-29 19:35:27 +0200

    Add files via upload

--- Memory/Engine.py
+++ Memory/Engine.py
@@ -1,25 +1,23 @@
 import pygame
-__name__="engine"
+__name__="Engine"
 
-def button(event, x, y, width, height, funkcija, button=1):
-    if event.type==pygame.MOUSEBUTTONDOWN:
-        if event.pos[0]>=x and event.pos[0]<=x+width and event.pos[1]>=y and event.pos[1]<=y+height and event.button==button:
-            funkcija()
-
-def buttonDraw(display, x, y, width, height, background_color, text="", font_size=20, text_color=[0,0,0], border=0, border_color=[0,0,0]):
-    pygame.draw.rect(display, border_color, [x-border, y-border, width+2*border, height+2*border])
-    pygame.draw.rect(display, background_color, [x, y, width, height])
+def button(display, x, y, width, height, funkcija,varijable, background_color="", disabled=False,  image="", title="", title_size=20, title_color=[255,255,255], text="", font_size=20, text_color=[0,0,0], border=0, border_color=[0,0,0], button=0):
+    if pygame.mouse.get_pressed()[button]:
+        mpos=pygame.mouse.get_pos()
+        if mpos[0]>=x and mpos[0]<=x+width and mpos[1]>=y and mpos[1]<=y+height and not disabled:
+            funkcija(varijable)
+    if background_color!="":
+        pygame.draw.rect(display, border_color, [x-border, y-border, width+2*border, height+2*border])
+        pygame.draw.rect(display, background_color, [x, y, width, height])
+    if image!="":
+        display.blit(image, [x,y])
     font = pygame.font.Font('freesansbold.ttf', font_size)
     text=font.render(text, True, text_color)
     textRect = text.get_rect()
     textRect.center = [x+width/2, y+height/2]
     display.blit(text, textRect)
-
-def buttonDrawImage(display, x, y, image, title="", font_size=20, title_color=[255,255,255]):
-    display.blit(image, [x,y])
-    font = pygame.font.Font('freesansbold.ttf', font_size)
+    font = pygame.font.Font('freesansbold.ttf', title_size)
     text=font.render(title, True, title_color)
     textRect=text.get_rect()
-    textRect.center=[x+image.get_width()/2, y+image.get_height()+textRect.height]
-    display.blit(image, [x, y])
+    textRect.center=[x+width/2, y+height+textRect.height]
     display.blit(text, textRect)
--- Memory/Memory.py
+++ Memory/Memory.py
@@ -1,69 +1,108 @@
 import Engine #umetanje Engine, napravio ga je Leonardo
 import pygame #umetanje pygamea
+import time #umetanje time
+import sys
 pygame.init()
 white=(255, 255, 255) #odredivanje boje za pozadinu
 #odredivanje velicine ekrana
-X = 1200 
-Y = 600
+X = 1140 
+Y = 540
 dis=pygame.display.set_mode((X, Y ))
 dis.fill(white)
+begtime=time.time()
 #umetanje slika
-red=pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\red.jpg')
-image0 = pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\flower.jpg') 
-image1 = pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\sun.jpg')
-image2 = pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\flower2.jpg')
-image3 = pygame.image.load(r'C:\Users\User\Downloads\Programiranje\Memory\flower3.jpg')
+red=pygame.image.load('red.jpg')
+image0 = pygame.image.load('flower.jpg') 
+image1 = pygame.image.load('sun.jpg')
+image2 = pygame.image.load('flower2.jpg')
+image3 = pygame.image.load('flower3.jpg')
 #random mjesanje slika
 l=[image0, image1, image2, image3, image0, image1, image2, image3]
 import random
-n=random.choice(l)
-l.remove(n)
-a=random.choice(l)
-l.remove(a)
-b=random.choice(l)
-l.remove(b)
-c=random.choice(l)
-l.remove(c)
-d=random.choice(l)
-l.remove(d)
-e=random.choice(l)
-l.remove(e)
-f=random.choice(l)
-l.remove(f)
-g=random.choice(l)
-l.remove(g)
-def fun():
-    print('nom')
+li=[]
+for i in range(8):
+    li.append(random.choice(l))
+    l.remove(li[i])
 
-#postavljanje drge strane 'kartice' preko slika tako da ih igrac ne vidi
+show=[True,True,True,True,True,True,True,True]
+disabled=False
+open_card=False
+forever_yeet=[True,True,True,True,True,True,True,True]
+card1=0
+def otvori(broj_kartice):
+    global open_card, disabled, begtime, card1
+    if open_card:
+        open_card=False
+        if li[broj_kartice]==li[card1]:
+            forever_yeet[broj_kartice]=False
+            forever_yeet[card1]=False
+        else:
+            disabled=True
+            begtime=time.time()
+    else:
+        open_card=True
+        card1=broj_kartice
+    show[broj_kartice]=False
+def fun():
+    Engine.button(dis, 10, 10, red.get_width(), red.get_height(),otvori,0,'',disabled, red)
+def fun1():
+    Engine.button(dis, 10, 300,red.get_width(), red.get_height(),otvori,1,'',disabled, red)
+def fun2():
+    Engine.button(dis,300, 10,red.get_width(), red.get_height(),otvori,2,'',disabled, red)
+def fun3():
+    Engine.button(dis,600, 10,red.get_width(), red.get_height(),otvori,3,'',disabled, red)
+def fun4():
+    Engine.button(dis,300, 300,red.get_width(), red.get_height(),otvori,4,'',disabled, red)
+def fun5():
+    Engine.button(dis,600, 300,red.get_width(), red.get_height(),otvori,5,'',disabled, red)
+def fun6():
+    Engine.button(dis,900, 10,red.get_width(), red.get_height(),otvori,6,'',disabled, red)
+def fun7():
+    Engine.button(dis,900, 300,red.get_width(), red.get_height(),otvori,7,'',disabled, red)
+def slike_display(): #postavljanje izmjesanih slika na pozicije
+    dis.blit(li[0], (10, 10))
+    dis.blit(li[1], (10, 300))
+    dis.blit(li[2], (300, 10))
+    dis.blit(li[3], (600, 10))
+    dis.blit(li[4], (300, 300))
+    dis.blit(li[5], (600, 300))
+    dis.blit(li[6], (900, 10))
+    dis.blit(li[7], (900, 300))
+def kartice():
+    global show
+    if show[0] and forever_yeet[0]:
+        fun()
+    if show[1] and forever_yeet[1]:   
+        fun1()
+    if show[2] and forever_yeet[2]:   
+        fun2()
+    if show[3] and forever_yeet[3]:   
+        fun3()
+    if show[4] and forever_yeet[4]: 
+        fun4()
+    if show[5] and forever_yeet[5]:   
+        fun5()
+    if show[6] and forever_yeet[6]:   
+        fun6()
+    if show[7] and forever_yeet[7]:  
+        fun7()
 
 #programa
 while True:
+    if begtime+3<time.time() and not open_card:
+        disabled=False
+        show=[True,True,True,True,True,True,True,True]
+    dis.fill(white) #bjela pozadina
+    #postavljanje slika na pozicije
+    slike_display()
+    #postavljanje druge strane 'kartice' preko slika tako da ih igrac ne vidi
+    kartice()
+    pygame.display.update()
     for event in pygame.event.get() :
-            dis.fill(white) #bijela pozadina
-            #postavljanje slika na pozicije
-            dis.blit(n, (10, 10))
-            dis.blit(a, (10, 300))
-            dis.blit(b, (300, 10))
-            dis.blit(c, (600, 10))
-            dis.blit(d, (300, 300))
-            dis.blit(e, (600, 300))
-            dis.blit(f, (900, 10))
-            dis.blit(g, (900, 300))
-            #postavljanje drge strane 'kartice' preko slika tako da ih igrac ne vidi
-            Engine.buttonDrawImage(dis, 10, 10, red)
-            Engine.buttonDrawImage(dis, 10, 300, red)
-            Engine.buttonDrawImage(dis, 300, 10, red)
-            Engine.buttonDrawImage(dis, 600, 10, red)
-            Engine.buttonDrawImage(dis, 300, 300, red)
-            Engine.buttonDrawImage(dis, 600, 300, red)
-            Engine.buttonDrawImage(dis, 900, 10, red)
-            Engine.buttonDrawImage(dis, 900, 300, red)
-            Engine.button(event,10,10,250,250,fun,button=1)
-            if event.type==pygame.QUIT:
-                pygame.quit()
-                quit()
-            pygame.display.update()
+        if event.type==pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+
 
 
 
